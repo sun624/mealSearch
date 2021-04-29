0907f49e7c5879890520854eac8dbf1e8c21348a
sun624 <67940809+sun624@users.noreply.github.com>
2021-04-28 20:03:07 -0700

    init commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const cors = require("cors");
+// if (!process.env.PORT) {
+//   require("./Secrets");
+// }
+const MongoClient = require("mongodb").MongoClient;
+
+const app = express();
+app.use(cors());
+app.use(express.json()); //data from json
+app.use(express.urlencoded({ extended: true })); //data from form
+app.use(express.static("public"));
+
+const path = require("path");
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server is listening on ${PORT}.`);
+});
+
+app.get("/auth", (req, res) => {
+  res.sendFile(path.join(__dirname + "/auth.html"));
+});
